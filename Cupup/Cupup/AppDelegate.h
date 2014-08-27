f31b3ddbffc396904f17f8abccef7eeff97c0165
anyi <anyi@meilizhuanjia.cn>
2014-08-27 18:52:27 +0800

    create project

---
+++ Cupup/Cupup/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Cupup
+//
+//  Created by aypc on 14-8-27.
+//  Copyright (c) 2014年 aypc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Cupup/Cupup/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Cupup
+//
+//  Created by aypc on 14-8-27.
+//  Copyright (c) 2014年 aypc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
