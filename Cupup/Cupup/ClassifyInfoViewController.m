e76cfeddfb8e098645fe7aa11b9224686c39da47
anyi <anyi@meilizhuanjia.cn>
2014-09-15 22:03:19 +0800

    add file

---
+++ Cupup/Cupup/ClassifyInfoViewController.m
@@ -0,0 +1,38 @@
+//
+//  ClassifyInfoViewController.m
+//  Cupup
+//
+//  Created by aypc on 14-9-15.
+//  Copyright (c) 2014年 aypc. All rights reserved.
+//
+
+#import "ClassifyInfoViewController.h"
+
+@interface ClassifyInfoViewController ()
+
+@end
+
+@implementation ClassifyInfoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
